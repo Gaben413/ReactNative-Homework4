a8de882bbcf18016b27c8d961fcff9dc9fcae854
NowhereCat <gabrielbportela413@gmail.com>
2023-03-16 18:34:18 -0300

    Revert "Update App.js"
    This reverts commit d377f71332573f9ff37d3d55af68d669cfa41b06.

--- my-project/App.js
+++ my-project/App.js
@@ -1,42 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState} from "react";
+import React, {useState} from "react"
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export default function App() {
 
-  const [text1, setText1] = useState('Empty')
-  const [text2, setText2] = useState('24')
-  const [_bool, setBool] = useState(false)
+  const [text, setText] = useState('Empty')
 
   return (
     <View style={styles.container}>
       
-      <Text style={styles.titleLogin}>Login</Text>
+      <Text>Digite um nome:</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setText}
+        placeholder='Text'
+        value={text}
+      />
 
-      <View style={styles.innerContainer}>
-        <Ionicons name="ios-mail" size={20} color="#000" />
-        <TextInput
-          style={styles.input}
-          onChangeText={setText1}
-          placeholder='Enter your email'
-        />
-      </View>
-      
-      <View style={styles.innerContainer}>
-        <Ionicons name="key" size={20} color="#000" />
-        <TextInput
-          secureTextEntry={true}
-          style={styles.input}
-          onChangeText={setText2}
-          placeholder='Enter your password'
-        />
-      </View>
+      <Text>Digite a idade:</Text>
+      <TextInput
+        keyboardType='numeric'
+        style={styles.input}
+        onChangeText={setText}
+        placeholder='Text'
+        value={text}
+      />
 
-      <View style={styles.buttonView}>
-        <Button title='LOGIN' color='#fc8632' />
-      </View>
-      
+      <Button 
+        title='Apresentar Texto'
+      />
+
+      {
+        text === 'Ligado' ?
+        <Text>Apresentando Texto</Text>
+        :
+        <Text></Text>
+      }
 
       <StatusBar style="auto" />
     </View>
@@ -50,35 +49,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  titleLogin:
-  {
-    fontSize: 15,
-  },
-  innerContainer:
-  {
-    flexDirection: 'row',
-    gap: 5,
-    alignItems: 'center',
-    width: '85%',
-    borderWidth: 2,
-    borderRadius: 15,
-    paddingLeft: 5,
-    margin: 10,
-
-  },
-  input:{
-    minWidth: 200,
-    minHeight: 50,
-    paddingLeft: 5,
-  },
-  button:
-  {
-    width: 500,
-  },
-  buttonView:
-  {
-    width: '50%',
-
-    alignItems: 'stretch'
-  }
 });
